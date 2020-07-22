2fb3ba17a7268134b5e35ca4ced073598f327c36
rodrigodsluz <rodrigodsluz@gmail.com>
2020-07-22 13:56:12 -0300

    Add ex02

--- create-server/ex02.js
+++ create-server/ex02.js
@@ -1,10 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  //console.log(req)
-  //process.exit()
-
-  console.log(req.url, req.method, req.headers)
+  const url = req.url
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>')
   res.write('<head><title>Node.JS</title></head>')
