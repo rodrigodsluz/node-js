9b73822b500de4046c464a8a6c6467d3734370e8
rodrigodsluz <rodrigodsluz@gmail.com>
2020-07-27 20:04:08 -0300

    Requests

--- response.js
+++ response.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   const url = req.url
   const method = req.method
 
-  if (url === '/') {
+  if (url === '/') { 
     res.write('<html>')
     res.write('<head><title>Enter message</title></head>')
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >Send</button></form></body>')
