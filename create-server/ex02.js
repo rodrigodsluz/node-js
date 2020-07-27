317712ec0d010d927355e2fa2c5caf01ff48f5a7
rodrigodsluz <rodrigodsluz@gmail.com>
2020-07-27 19:23:50 -0300

    Redirecting requests

--- create-server/ex02.js
+++ create-server/ex02.js
@@ -1,7 +1,10 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   const url = req.url
+  const method = req.method
+
   if (url === '/') {
     res.write('<html>')
     res.write('<head><title>Enter message</title></head>')
@@ -9,6 +12,15 @@ const server = http.createServer((req, res) => {
     res.write('</html>')
     return res.end()
   }
+
+  if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.txt', 'DUMMY')
+    //res.writeHead(302, 'Location', '/')
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end()
+  }
+
   res.setHeader('Content-Type', 'text/html')
   res.write('<html>')
   res.write('<head><title>Node.JS</title></head>')
