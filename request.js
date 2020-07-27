6465818548dfe8a47efc6c8c549d1e44a3d741bf
rodrigodsluz <rodrigodsluz@gmail.com>
2020-07-27 20:04:59 -0300

    Understand requests

--- request.js
+++ request.js
@@ -11,6 +11,6 @@ const server = http.createServer((req, res) => {
   res.write('<body><h1>Hello from my Node.JS server!</h1></body>')
   res.write('</html>')
   res.end()
-})
+}) 
 
 server.listen(8000)
