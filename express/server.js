beef94d2e3a9842adb0a49f0cde14ec9c7f6daae
rodrigodsluz <rodrigodsluz@gmail.com>
2020-09-02 09:08:07 -0300

    Install express

--- express/server.js
+++ express/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
+app.use('/', (req, res, next) => {
+  console.log('THis always run');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>The "Add product" Page</h1>')
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from Express</h1>')
 });
