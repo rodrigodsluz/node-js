47cad5d9e9a9b63451d307266fff33644e222f7b
rodrigodsluz <rodrigodsluz@gmail.com>
2020-09-02 09:01:22 -0300

    Add middleware

--- express/server.js
+++ express/server.js
@@ -1,9 +1,15 @@
-const http = require('http')
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
-  
-server.listen(8000)
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from Express</h1>')
+});
+
+app.listen(8000);
